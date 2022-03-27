fc74dec0ceed291002244d4e587b7532cf191add
TechReport2022 <86839339+TechReport2022@users.noreply.github.com>
2022-03-27 17:49:33 +0800

    Add files via upload

---
+++ topKDiversifiedMiner/model/Edge.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Edge {
+    public int from = 0;
+    public int to = 0;
+    public int eLabel = 0;
+    public int id = 0;
+    
+    
+}
---
+++ topKDiversifiedMiner/model/PDFS.java
@@ -0,0 +1,8 @@
+package model;
+
+public class PDFS {
+    // ID of the original input graph
+    public int id = 0;
+    public Edge edge = null;
+    public PDFS prev = null;
+}
